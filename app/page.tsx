3a99692e02f6a1068e08023839cc12a330a5fc04
fareedzafar657 <fareedzafar657@gmail.com>
2025-05-04 16:06:54 +0500

    joggers model added

--- app/page.tsx
+++ app/page.tsx
@@ -8,20 +8,22 @@ import ShoeModel from "@/components/ShoeModel";
 
 export default function JoggersPage() {
   return (
-    <div className="min-h-screen relative overflow-hidden bg-slate-50/30 backdrop-blur-sm">
-      <Navbar />
+    <div className="bg-slate-50/30 backdrop-blur-sm">
+      <ShoeModel />
+      <div className="min-h-screen relative overflow-hidden ">
+        <Navbar />
 
-      <main className=" px-16 py-8 flex">
-        <div className="space-y-8 self-center">
-          <Content />
-          <Features />
-        </div>
+        <main className=" px-16 py-8 flex">
+          <div className="space-y-8 self-center">
+            <Content />
+            <Features />
+          </div>
 
-        <div className="w-full self-end">
-          <ShoeModel />
-          <Specs />
-        </div>
-      </main>
+          <div className="w-full self-end">
+            <Specs />
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
--- components/ShoeModel.tsx
+++ components/ShoeModel.tsx
@@ -1,17 +1,56 @@
-import React from "react";
+import React, { Suspense, useEffect, useRef } from "react";
 import { Canvas } from "@react-three/fiber";
+import { useGLTF } from "@react-three/drei";
+import gsap from "gsap";
+
+const Shoe = () => {
+  const { scene } = useGLTF("/joggers.glb");
+  const shoeRef = useRef(null);
+
+  useEffect(() => {
+    if (shoeRef.current) {
+
+      const tl = gsap.timeline();
+
+      tl.fromTo(
+        shoeRef.current.position,
+        { y: 5, x: 5 },
+        { y: 0, x: 0.5, duration: 1.5 }
+      )
+      // Floating animation after position animation
+      .to(shoeRef.current.position, {
+        y: 0.2, // Float up to y = 1
+        duration: 1.5,
+        yoyo: true,  // Make it go back down after reaching y = 1
+        repeat: -1,  // Loop infinitely
+        ease: "power1.inOut", // Smooth easing effect
+      });
+    }
+  }, []);
+
+  return (
+    <primitive
+      ref={shoeRef}
+      object={scene}
+      scale={2} 
+      position={[0.65, 0, 0.5]} 
+      rotation={[0, 0, 0.5]}
+    />
+  );
+};
 
 const ShoeModel = () => {
   return (
-    <div id="canvas-container">
-      <Canvas>
-      <ambientLight intensity={0.1} />
-      <directionalLight color="red" position={[0, 0, 5]} />
-        <mesh>
-          <boxGeometry args={[2, 2, 2]} />
-          <meshStandardMaterial />
-        </mesh>
-      </Canvas>
+    <div className="absolute">
+      <div className="h-screen w-screen">
+        <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
+          <ambientLight intensity={0.5} />
+          <directionalLight color="white" position={[5, 5, 5]} />
+          <Suspense fallback={null}>
+            <Shoe />
+          </Suspense>
+        </Canvas>
+      </div>
     </div>
   );
 };
